87844c2d8ace3a1dd1c275189cf28e7c1f66add5
mattsperez <131983739+mattsperez@users.noreply.github.com>
2024-02-16 11:34:22 -0600

    6.4

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -9,6 +9,10 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+// Takes user to MovieCardComponent
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
@@ -20,28 +24,33 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
 
   // This is the function responsible for sending the form inputs to the backend
   loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe(
-      (result) => {
-        // Logic for a successful user login goes here! (To be implemented)
-        this.dialogRef.close(); // This will close the modal on success!
-        console.log(result);
-        this.snackBar.open(result, 'OK', {
-          duration: 2000,
-        });
-      },
-      (result) => {
-        console.log(result);
-        this.snackBar.open(result, 'OK', {
-          duration: 2000,
-        });
-      }
-    );
-  }
+   console.log('Sending login request with data:', this.userData); // Log the data being sent
+   this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+    // Logic for a successful user login goes here! (To be implemented)
+    console.log(result);
+    localStorage.setItem('user', JSON.stringify(result.user));
+    localStorage.setItem('token', result.token);
+    this.dialogRef.close(); // This will close the modal on success!
+    console.log(result);
+    this.snackBar.open('Logged in', 'OK', {
+      duration: 2000
+    });
+    // Successfully login done
+
+    this.router.navigate(['movies/']);
+  }, (error) => {
+    console.error('Login failed:', error); // Log any error response
+    this.snackBar.open('Login failed', 'OK', {
+      duration: 2000
+    });
+  });
+}
 }
